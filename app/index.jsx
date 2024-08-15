df03bf8f128df1e59cc337891b0e320dcc0ceab4
manancodes <manancodes.dev@gmail.com>
2024-08-15 22:57:23 +0530

    feat: Added intro screen

---
+++ app/index.jsx
@@ -0,0 +1,108 @@
+import { View, Text, StyleSheet, Image, Pressable } from "react-native";
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { hp, wp } from "../helpers/common";
+import { LinearGradient } from "expo-linear-gradient";
+import Animated, { FadeInDown } from "react-native-reanimated";
+import { theme } from "../constants/theme";
+import { useRouter } from "expo-router";
+
+const WelcomeScreen = () => {
+  const router = useRouter();
+  return (
+    <View style={styles.container}>
+      <StatusBar style="light" />
+      <Image
+        source={require("../assets/images/baloons.png")}
+        style={styles.bgImage}
+        resizeMode="cover"
+      />
+      <Animated.View entering={FadeInDown.duration(600)} style={{ flex: 1 }}>
+        <LinearGradient
+          colors={[
+            "rgba(0,0,0,0)",
+            "rgba(0,0,0,0.5)",
+            "rgba(0,0,0,0.8)",
+            "black",
+          ]}
+          style={styles.gradient}
+          start={{ x: 0.5, y: 0 }}
+          end={{ x: 0.5, y: 0.8 }}
+        />
+        <View style={styles.contentContainer}>
+          <Animated.Text
+            entering={FadeInDown.delay(400).springify()}
+            style={styles.title}
+          >
+            Wellpaper
+          </Animated.Text>
+          <Animated.Text
+            entering={FadeInDown.delay(500).springify()}
+            style={styles.punchline}
+          >
+            Awesome outfits for your phone
+          </Animated.Text>
+          <Animated.View entering={FadeInDown.delay(600).springify()}>
+            <Pressable
+              // onPress={() => router.push("home")}
+              style={styles.startButton}
+            >
+              <Text style={styles.startText}>Explore</Text>
+            </Pressable>
+          </Animated.View>
+        </View>
+      </Animated.View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  bgImage: {
+    width: wp(100),
+    height: hp(100),
+    position: "absolute",
+  },
+  gradient: {
+    width: wp(100),
+    height: hp(65),
+    bottom: 0,
+    position: "absolute",
+  },
+  contentContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "flex-end",
+    gap: 14,
+  },
+  title: {
+    fontSize: hp(7),
+    color: theme.colors.neutral(0.9),
+    fontWeight: theme.fontWeights.bold,
+  },
+  punchline: {
+    fontSize: hp(2),
+    letterSpacing: 1,
+    marginBottom: 10,
+    fontWeight: theme.fontWeights.medium,
+    color: theme.colors.neutral(0.9),
+  },
+  startButton: {
+    marginBottom: 50,
+    backgroundColor: theme.colors.neutral(0.9),
+    padding: 15,
+    paddingHorizontal: 90,
+    borderRadius: theme.radius.sm,
+    borderCurve: "continuous",
+  },
+  startText: {
+    color: theme.colors.black,
+    fontSize: hp(3),
+    fontWeight: theme.fontWeights.medium,
+    letterSpacing: 1,
+  },
+});
+
+export default WelcomeScreen;
